e6772e613105c19303e1a191e1814fcb27b4202c
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-03-30 12:36:55 +0200

    Added NotificationManager

---
+++ Shared/NotificationManager.swift
@@ -0,0 +1,35 @@
+//
+//  NotificationManager.swift
+//  TimerNotifications
+//
+//  Created by Gregor Hermanowski on 30. March 2022.
+//
+
+import Foundation
+import UserNotifications
+import SwiftUI
+
+@MainActor final class NotificationManager: ObservableObject {
+	static let shared = NotificationManager()
+	
+	private init() { }
+	
+	let notificationCentre = UNUserNotificationCenter.current()
+	
+	@AppStorage("hasRequestedPermission") private(set) var hasRequestedPermission = false
+	
+	func canSendNotfications() async -> Bool {
+		let settings = await notificationCentre.notificationSettings()
+		return settings.authorizationStatus == .authorized
+	}
+	
+	func requestPermission() async {
+		do {
+			try await notificationCentre.requestAuthorization(options: [.alert, .badge, .sound])
+		} catch {
+			print(error)
+		}
+		
+		hasRequestedPermission = true
+	}
+}
