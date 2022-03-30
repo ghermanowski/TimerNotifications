1965f9b120c6259a0b8fb7a303285ef32cd3ecae
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-03-30 10:41:58 +0200

    Project setup 2

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerNotifications
+//
+//  Created by Gregor Hermanowski on 30. March 2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TimerNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerNotificationsApp.swift
+//  TimerNotifications
+//
+//  Created by Gregor Hermanowski on 30. March 2022.
+//
+
+import SwiftUI
+
+@main
+struct TimerNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
