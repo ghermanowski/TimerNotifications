ce3719b621318b392a99cfbd8ba197d0bf0854a2
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-04-01 13:15:00 +0200

    Used sorting from Comparable

--- Shared/PendingNotifications.swift
+++ Shared/PendingNotifications.swift
@@ -9,22 +9,7 @@ import SwiftUI
 
 struct PendingNotifications: View {
 	internal init(_ notifications: [UNNotificationRequest]) {
-		self.notifications = notifications.sorted { left, right in
-			guard let leftDate = (left.trigger as? UNCalendarNotificationTrigger)?.nextTriggerDate() ??
-			(left.trigger as? UNTimeIntervalNotificationTrigger)?.nextTriggerDate() else {
-				let rightDate = (right.trigger as? UNCalendarNotificationTrigger)?.nextTriggerDate() ??
-				(right.trigger as? UNTimeIntervalNotificationTrigger)?.nextTriggerDate()
-				
-				return rightDate != nil
-			}
-			
-			guard let rightDate = (right.trigger as? UNCalendarNotificationTrigger)?.nextTriggerDate() ??
-					(right.trigger as? UNTimeIntervalNotificationTrigger)?.nextTriggerDate() else {
-				return true
-			}
-			
-			return leftDate < rightDate
-		}
+		self.notifications = notifications.sorted()
 	}
 	
 	private let notifications: [UNNotificationRequest]
